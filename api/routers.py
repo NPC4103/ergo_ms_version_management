5cb53fb4296699608d5430b6c172280c41329c38
NPC4103 <konnikita.job410@gmail.com>
2026-01-20 16:00:56 +0300

    feat: Настроил модель, теперь создаёт миграции и направляет их в бд

--- api/routers.py
+++ api/routers.py
@@ -9,28 +9,33 @@ class AppLabelRouter:
     Для модуля version_management используется алиас БД 'version_management' из databases.yaml.
     """
 
-    app_label_to_db = {
-        'version_management': 'version_management',
-    }
+    # app_label_to_db = {
+    #     'version_management': 'version_management',
+    # }
 
-    def _get_db_for_app(self, app_label: str) -> Optional[str]:
-        return self.app_label_to_db.get(app_label)
-
+    route_app_labels = {'version_management'}
+    
     def db_for_read(self, model, **hints):
-        return self._get_db_for_app(model._meta.app_label)
+        if model._meta.app_label in self.route_app_labels:
+            return 'version_management'
+        return None
 
     def db_for_write(self, model, **hints):
-        return self._get_db_for_app(model._meta.app_label)
+        if model._meta.app_label in self.route_app_labels:
+            return 'version_management'
+        return None
 
     def allow_relation(self, obj1, obj2, **hints):
-        db1 = self._get_db_for_app(obj1._meta.app_label)
-        db2 = self._get_db_for_app(obj2._meta.app_label)
+        if (
+            obj1._meta.app_label in self.route_app_labels or
+            obj2._meta.app_label in self.route_app_labels
+        ):
         if db1 and db2:
-            return db1 == db2
+            return True
         return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
-        target_db = self._get_db_for_app(app_label)
-        if target_db:
+        if app_label in self.route_app_labels:
             return db == target_db
-        return None
+        else:
+            return db != target_db
