e386fbe5cacfa955105f2d52a386e9cff3697b70
Marshalltfmh <zkuzzzya@gmail.com>
2026-01-27 18:46:44 +0300

    fix: Исправлен вызов утилиты через ergovcs.cmd

--- deployment/ergovcs.cmd
+++ deployment/ergovcs.cmd
@@ -6,6 +6,7 @@ chcp 65001 >nul
 setlocal ENABLEDELAYEDEXPANSION
 set "SCRIPT_DIR=%~dp0"
 
-powershell -NoProfile -ExecutionPolicy Bypass -Command "[Console]::OutputEncoding=[System.Text.Encoding]::UTF8; [Console]::InputEncoding=[System.Text.Encoding]::UTF8; $OutputEncoding=[System.Text.Encoding]::UTF8; & '%SCRIPT_DIR%windows\version_manager.ps1' %*"
+REM Use -File to pass arguments verbatim; -Command can mangle quoting and args.
+powershell -NoProfile -ExecutionPolicy Bypass -File "%SCRIPT_DIR%windows\version_manager.ps1" %*
 
 endlocal
